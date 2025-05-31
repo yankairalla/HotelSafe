9dbc827d08af2cf8a1c3311d19c5e4b062b90e8f
Yan Kairalla <yanmkairalla@hotmail.com>
2025-05-31 02:43:06 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/yankairalla/HotelSafe/HotelSafeApplication.java
@@ -0,0 +1,13 @@
+package com.yankairalla.HotelSafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSafeApplication.class, args);
+	}
+
+}

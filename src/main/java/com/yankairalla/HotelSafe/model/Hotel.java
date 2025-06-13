bea5c3469f6b1da619c3ea611b945341beba0e5e
Yan Kairalla <yanmkairalla@hotmail.com>
2025-06-13 04:41:04 -0300

    Adiciona validação

--- src/main/java/com/yankairalla/HotelSafe/model/Hotel.java
+++ src/main/java/com/yankairalla/HotelSafe/model/Hotel.java
@@ -2,6 +2,12 @@ package com.yankairalla.HotelSafe.model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalTime;
 import java.util.List;
@@ -11,16 +17,39 @@ public class Hotel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotBlank(message = "Nome é obrigatório.")
     private String name;
+
+    @Email(message = "Email deve ser valido")
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+
+    @NotBlank(message = "Endereço é obrigatório.")
     private String address;
+
+    @NotBlank(message = "Cidade é obrigatório.")
     private String city;
+
+    @NotBlank(message = "CNPJ é obrigatório")
+    @Pattern(regexp = "\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}|\\d{14}",
+            message = "CNPJ deve estar no formato 00.000.000/0000-00")
     private String cnpj;
+
+    @NotBlank(message = "Telefone é obrigatório")
+    @Pattern(regexp = "\\(\\d{2}\\)\\s\\d{4,5}-\\d{4}",
+            message = "Telefone deve estar no formato (00) 00000-0000")
     private String phone;
+
     @OneToMany(mappedBy = "hotel")
     private List<Quarto> quartos;
+
+    @NotNull(message = "Check-in é obrigatório")
     private LocalTime checkIn;
+
+    @NotNull(message = "Check-out é obrigatório")
     private LocalTime checkOut;
     private Boolean active;
+//    private List<MultipartFile> photos;
 
     public String getName() {
         return name;
@@ -97,4 +126,12 @@ public class Hotel {
     public void setActive(Boolean active) {
         this.active = active;
     }
+
+//    public List<MultipartFile> getPhotos() {
+//        return photos;
+//    }
+//
+//    public void setPhotos(List<MultipartFile> photos) {
+//        this.photos = photos;
+//    }
 }
